00e0310574b0e21c86da64fb28c54eaf6a08872e
Jack <dudeobama246@gmail.com>
2023-05-26 23:02:27 +0530

    First Commit

---
+++ Learning_Git/src/com/learning_git/space/Learn_Github.java
@@ -0,0 +1,10 @@
+package com.learning_git.space;
+
+public class Learn_Github {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("I am Waqhar Hussain!!! Learning GITHUB...");
+	}	
+
+}
