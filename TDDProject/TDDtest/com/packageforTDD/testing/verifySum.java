957d0f18c5e53384640932fdf415a403e3d6f369
Jack <dudeobama246@gmail.com>
2023-05-28 00:11:40 +0530

    My 8th Commit

--- TDDProject/TDDtest/com/packageforTDD/testing/verifySum.java
+++ TDDProject/TDDtest/com/packageforTDD/testing/verifySum.java
@@ -1,6 +1,10 @@
 package com.packageforTDD.testing;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class verifySum {
+	private List<Integer> list = new ArrayList<>();
 	public long checkSum(String str) {
 		if(str.isEmpty()) return 0;
 		
@@ -8,51 +12,46 @@ public class verifySum {
 		boolean newLineTag = str.contains("\n");
 		boolean comma = str.contains(",");
 		
-		long sum = 0, cnt = 0;
+		long sum = 0;
 		if(doubleSlach) {
 			String nstr = str.replace("//", "").replace(";", "").replace("\n", "");
 			for(char c : nstr.toCharArray()) {
 				if(Character.getNumericValue(c) < 0) {
-					cnt++;
-		
+					list.add(Character.getNumericValue(c));
 				}
 				sum += Character.getNumericValue(c);
 			}
-			if(cnt == 1) throw new IllegalArgumentException("Negatives are not allowed");
+			if(list.size() == 1) throw new IllegalArgumentException("Negatives are not allowed");
 		}
 		if(comma && newLineTag) {
 			String nstr = str.replace(",", "").replace("\n", "");
 			for(char c : nstr.toCharArray()) {
 				if(Character.getNumericValue(c) < 0) {
-					cnt++;
-					throw new IllegalArgumentException("Negatives are not allowed");
+					list.add(Character.getNumericValue(c));
 				}
 				sum += Character.getNumericValue(c);
 			}
-			if(cnt == 1) throw new IllegalArgumentException("Negatives are not allowed");
+			if(list.size() == 1) throw new IllegalArgumentException("Negatives are not allowed");
 		}
 		else if(newLineTag) {
 			String nstr = str.replace("\n", "");
 			for(char c : nstr.toCharArray()) {
 				if(Character.getNumericValue(c) < 0) {
-					cnt++;
-					throw new IllegalArgumentException("Negatives are not allowed");
+					list.add(Character.getNumericValue(c));
 				}
 				sum += Character.getNumericValue(c);
 			}
-			if(cnt == 1) throw new IllegalArgumentException("Negatives are not allowed");
+			if(list.size() == 1) throw new IllegalArgumentException("Negatives are not allowed");
 		}
 		else if(comma) {
 			String nstr = str.replace(",", "");
 			for(char c : nstr.toCharArray()) {
 				if(Character.getNumericValue(c) < 0) {
-					cnt++;
-					
-					throw new IllegalArgumentException("Negatives are not allowed");
+					list.add(Character.getNumericValue(c));
 				}
 				sum += Character.getNumericValue(c);
 			}
-			if(cnt == 1) throw new IllegalArgumentException("Negatives are not allowed");
+			if(list.size() == 1) throw new IllegalArgumentException("Negatives are not allowed");
 		}
 		
 		return sum;
