1980840e2323f9540f2fc1357da2ad4c28a3f5fe
Jack <dudeobama246@gmail.com>
2023-05-27 16:19:02 +0530

    second commit on improvement

--- TDDProject/TDDtest/com/packageforTDD/testing/verifySum.java
+++ TDDProject/TDDtest/com/packageforTDD/testing/verifySum.java
@@ -4,6 +4,14 @@ import java.util.ArrayList;
 public class verifySum {
 	public boolean checkSum(String str) {
 		if(str.isEmpty()) return false;
+		
+		String[] numString = str.split(",");
+		int total = 0, onum = 0;
+		for(int i=0; i<numString.length; ++i) {
+			onum = Integer.parseInt(numString[i]);
+			total += onum;
+		}
+		if(total < 0) return false;
 		return true;
 	}
 }
